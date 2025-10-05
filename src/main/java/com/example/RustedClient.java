cf63bb324f846543c63a7773c4b07c31f1424c6b
wheeelee <sashashurillo@gmail.com>
2025-10-05 14:06:37 +1000

    еееее

---
+++ src/main/java/com/example/RustedClient.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RustedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
