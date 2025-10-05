17ec1547cd43e14d3238c66dc33aa36ff18f4fe2
wheeelee <sashashurillo@gmail.com>
2025-10-05 14:14:10 +1000

    comments

--- src/main/java/com/example/Rusted.java
+++ src/main/java/com/example/Rusted.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//УРА
 public class Rusted implements ModInitializer {
 	public static final String MOD_ID = "rusted";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
