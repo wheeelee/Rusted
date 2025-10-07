318eed15fe32f84e6007d0e2baeab7b238b57a99
wheeelee <sashashurillo@gmail.com>
2025-10-07 15:20:17 +1000

    added block

---
+++ src/main/java/com/example/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package com.example.block;
+
+import com.example.Rusted;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    private static final Block CLEAR_CRYSTAL_BLOCK = registerBlock("clean_crystal_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(Rusted.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(Rusted.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        Rusted.LOGGER.info("Registering Mod Blocks for " + Rusted.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(CLEAR_CRYSTAL_BLOCK);
+        });
+    }
+}
